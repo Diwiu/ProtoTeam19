17c5163b1595ffbb6752bd99cfa74e991b39049f
Diwiu <dani.lopez93@yahoo.com>
2022-10-07 09:50:07 +0200

    Fix health

---
+++ FutureGames_3CWorkshop/Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+
+    // public static bool GameIsPaused = false;
+    //
+    // private void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.P))
+    //     {
+    //         throw new NotImplementedException();
+    //     }
+    // }
+    //
+    // void Pause()
+    // {
+    //     pauseMenuUI.SetActive(true);
+    //     Time.timeScale = 0f;
+    //     GameIsPaused = true;
+    // }
+}
